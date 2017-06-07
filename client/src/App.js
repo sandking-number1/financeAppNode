a17d314806529010e40e9b8f714ef269aa170960
sandking-number1 <Alfred.J9412@gmail.com>
2017-06-07 23:54:29 +1000

    Check in

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+    }
+  }
+
+  renderItem(val) {
+    return <Item value={val} loggedIn={this.state.loggedIn} />
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Logged in? - {this.state.loggedIn}. To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>
+          {this.renderItem("Hello")}
+        </ul>
+        <button onClick={() => this.setState({ loggedIn: true })} />
+      </div>
+    );
+  }
+}
+
+class Item extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
+  flipState() {
+    console.log(this.state);
+    if (this.state.value === 'X') {
+      this.setState({ value: 'X' });
+    } else {
+      this.setState({ value: 'Y' });      
+    }
+  }
+
+  render() {
+    return (
+      <li onClick={() => this.flipState()}>{this.props.value} - {this.state.value}</li>
+    )
+  }
+}
+
+export default App;
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express page' });
+
+  res.json([{
+    id: 1,
+    username: "samsepi0l"
+  }, {
+    id: 2,
+    username: "D0loresH4ze"
+  }])
+});
+
+module.exports = router;
